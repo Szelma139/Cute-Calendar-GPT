e848a6d64b41268ea3390fbb0866d59aff9806e1
Radoslaw Niedbala <rniedbala@opgksoftware.com>
2023-07-02 12:41:13 +0200

    up

--- src/components/CalendarWidget.tsx
+++ src/components/CalendarWidget.tsx
@@ -1,103 +1,99 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import './CalendarWidget.module.css';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
+
+const BASE64_ENCODED_ICON =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAJUlEQVR42mNgAILy8vL/DLgASBKnApgkVgXIkhgKiNKJ005s4gDLbCZBiSxfygAAAABJRU5ErkJggg==';
+const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 
 const CalendarWidget = () => {
-  const [currentDay, setCurrentDay] = useState(new Date());
-  const calendarRef = useRef(null);
-  const [currentMonth, setCurrentMonth] = useState('');
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-  useEffect(() => {
-    const options: Intl.DateTimeFormatOptions = {
-      month: 'long',
-      year: 'numeric',
-    };
-    setCurrentMonth(currentDay.toLocaleDateString('en-US', options));
-  }, [currentDay]);
+  const getDayClassName = (day) => {
+    const dayOfWeek = day.getDay();
+    if (dayOfWeek === 0)
+      return `day sunday${
+        day.toDateString() === currentDate.toDateString() ? ' current-day' : ''
+      }`;
+    if (dayOfWeek === 6)
+      return `day saturday${
+        day.toDateString() === currentDate.toDateString() ? ' current-day' : ''
+      }`;
+    if (day.toDateString() === currentDate.toDateString())
+      return 'day current-day';
+    return 'day';
+  };
 
   const scrollCalendar = () => {
-    const calendarElement = calendarRef.current;
+    const calendarElement = document.getElementById('calendar');
     if (calendarElement) {
       const currentDayElement = calendarElement.querySelector('.current-day');
       if (currentDayElement) {
         const scrollLeft =
           currentDayElement.offsetLeft -
           (calendarElement.offsetWidth - currentDayElement.offsetWidth) / 2;
-        calendarElement.scrollTo({
-          left: scrollLeft,
-          behavior: 'smooth',
-        });
+
+        calendarElement.scrollLeft = scrollLeft;
       }
     }
   };
 
+  const handlePrevDay = () => {
+    const prevDay = new Date(currentDate);
+    prevDay.setDate(prevDay.getDate() - 1);
+    setCurrentDate(prevDay);
+    scrollCalendar();
+  };
+
   const handleNextDay = () => {
-    const nextDay = new Date(currentDay);
-    nextDay.setDate(currentDay.getDate() + 1);
-    setCurrentDay(nextDay);
+    const nextDay = new Date(currentDate);
+    nextDay.setDate(nextDay.getDate() + 1);
+    setCurrentDate(nextDay);
     scrollCalendar();
   };
 
-  const handlePreviousDay = () => {
-    const previousDay = new Date(currentDay);
-    previousDay.setDate(currentDay.getDate() - 1);
-    setCurrentDay(previousDay);
-    scrollCalendar();
-  };
-
-  const renderDay = (day) => {
-    const dayOptions = { weekday: 'short', month: 'short', day: 'numeric' };
-    const isCurrentDay = day.toDateString() === currentDay.toDateString();
-
-    return (
-      <div
-        key={day.toISOString()}
-        className={`day ${isCurrentDay ? 'current-day' : ''}`}
-      >
-        <span>{day.toLocaleDateString(undefined, dayOptions)}</span>
-      </div>
-    );
-  };
-
-  const renderCalendar = () => {
+  const renderDays = () => {
     const days = [];
-    const startDay = new Date(currentDay);
-    startDay.setDate(currentDay.getDate() - 3);
 
-    for (let i = 0; i < 7; i++) {
-      const day = new Date(startDay);
-      day.setDate(startDay.getDate() + i);
-      days.push(renderDay(day));
+    for (let i = -3; i <= 3; i++) {
+      const day = new Date(currentDate);
+      day.setDate(day.getDate() + i);
+
+      const className = getDayClassName(day);
+      const dayOfWeek = daysOfWeek[day.getDay()];
+      const visible = i === 0;
+
+      days.push(
+        <div className={className} key={day}>
+          {visible && <div className="arrow-down" />}
+          {day.getDate()}
+          <span className="day-of-week">{dayOfWeek}</span>
+        </div>
+      );
     }
 
-    return (
-      <div className="calendar" ref={calendarRef}>
-        {days}
-      </div>
-    );
+    return days;
   };
 
+  const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+
   return (
-    <div>
-      <div className="current-month">{currentMonth}</div>
-      <div className="calendar-container">
-        <button
-          type="button"
-          className="arrow left-arrow"
-          onClick={handlePreviousDay}
-        >
-          &lt;
-        </button>
-        {renderCalendar()}
-        <button
-          type="button"
-          className="arrow right-arrow"
-          onClick={handleNextDay}
-        >
-          &gt;
-        </button>
+    <div className="calendar-container">
+      <div className="arrow left-arrow" onClick={handlePrevDay} />
+      <div id="calendar" className="calendar">
+        {renderDays()}
       </div>
+      <div className="arrow right-arrow" onClick={handleNextDay} />
+      <div className="current-month">{currentMonth}</div>
     </div>
   );
 };
